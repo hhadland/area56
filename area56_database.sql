b4fb8e43fdb3f35695aa9912e9e38197601bbc0d
Casey Hawkins <hawkcase@flip3.engr.oregonstate.edu>
2021-07-25 17:58:34 -0700

    changed cl.sql file

--- area56_database.sql
+++ area56_database.sql
@@ -142,34 +142,35 @@ CREATE TABLE supply_products(
 --
 --
 
-INSERT INTO contacts VALUES (1, "Elvis", "Presley", "epresley@aol.com", "323-555-5555");
-INSERT INTO contacts VALUES (2, "Elvis", "Costello", "ecostello@aol.com", "310-222-2222");
-INSERT INTO contacts VALUES (3, "Buddy", "Holly", "bholly@gmail.com", "323-191-9151");
+INSERT INTO contacts (fname, lname, email, phone) VALUES ("Elvis", "Presley", "epresley@aol.com", "323-555-5555");
+INSERT INTO contacts (fname, lname, email, phone) VALUES ("Elvis", "Costello", "ecostello@aol.com", "310-222-2222");
+INSERT INTO contacts (fname, lname, email, phone) VALUES ("Buddy", "Holly", "bholly@gmail.com", "323-191-9151");
+INSERT INTO contacts (fname, lname, email, phone) VALUES ("Taylor", "Swift", "tswift@gmail.com", "323-654-7888");
 
-INSERT INTO sales_representatives VALUES (1, "John", "Lennon", "jlennon@gmail.com", "323-616-6626");
-INSERT INTO sales_representatives VALUES (2, "Paul", "McCartney", "pmccartney@gmail.com", "323-881-1888");
-INSERT INTO sales_representatives VALUES (3, "Ringo", "Starr", "rstarr@gmail.com", "323-255-5225");
+INSERT INTO sales_representatives (fname, lname, email, phone) VALUES ("John", "Lennon", "jlennon@gmail.com", "323-616-6626");
+INSERT INTO sales_representatives (fname, lname, email, phone) VALUES ("Paul", "McCartney", "pmccartney@gmail.com", "323-881-1888");
+INSERT INTO sales_representatives (fname, lname, email, phone) VALUES ("Ringo", "Starr", "rstarr@gmail.com", "323-255-5225");
 
-INSERT INTO clients VALUES (1, "Big Grocers", "151 North Ln, Des Moines, IA 50061", "biggrocers@gmail.com", "515-789-9987", 1, 1);
-INSERT INTO clients VALUES (2, "Best Stuff", "997 West Dr, Houston, TX 77002", "beststuff@gmail.com", "281-753-1159", 2, 2);
-INSERT INTO clients VALUES (3, "Wholesome Foods", "636 Downtown Blvd, San Francisco, CA 94108", "wholesomefoods@gmail.com", "415-395-2252", 3, 3);
+INSERT INTO clients (name, address, email, phone, contactID, repID) VALUES ("Big Grocers", "151 North Ln, Des Moines, IA 50061", "biggrocers@gmail.com", "515-789-9987", 1, 1);
+INSERT INTO clients (name, address, email, phone, contactID, repID) VALUES ("Best Stuff", "997 West Dr, Houston, TX 77002", "beststuff@gmail.com", "281-753-1159", 2, 2);
+INSERT INTO clients (name, address, email, phone, contactID, repID) VALUES ("Wholesome Foods", "636 Downtown Blvd, San Francisco, CA 94108", "wholesomefoods@gmail.com", "415-395-2252", 3, 3);
 
-INSERT INTO orders VALUES (1, 1, 1);
-INSERT INTO orders VALUES (2, 2, 2);
-INSERT INTO orders VALUES (3, 3, 3);
+INSERT INTO orders (repID, clientID) VALUES (1, 1, 1);
+INSERT INTO orders (repID, clientID) VALUES (2, 2, 2);
+INSERT INTO orders (repID, clientID) VALUES (3, 3, 3);
 
-INSERT INTO products VALUES (1, "Banana", 1);
-INSERT INTO products VALUES (2, "Apple", 1);
-INSERT INTO products VALUES (3, "Chicken", 10);
-INSERT INTO products VALUES (4, "Salmon", 20);
-INSERT INTO products VALUES (5, "Bread", 3);
-INSERT INTO products VALUES (6, "Milk", 4);
-INSERT INTO products VALUES (7, "Carton of Eggs", 2);
-INSERT INTO products VALUES (8, "Coffee Beans", 8);
-INSERT INTO products VALUES (9, "Broccoli", 3);
-INSERT INTO products VALUES (10, "Carrots", 2);
+INSERT INTO products (tem, cost) VALUES ("Banana", 1);
+INSERT INTO products (tem, cost) VALUES ("Apple", 1);
+INSERT INTO products (tem, cost) VALUES ("Chicken", 10);
+INSERT INTO products (tem, cost) VALUES ("Salmon", 20);
+INSERT INTO products (tem, cost) VALUES ("Bread", 3);
+INSERT INTO products (tem, cost) VALUES ("Milk", 4);
+INSERT INTO products (tem, cost) VALUES ("Carton of Eggs", 2);
+INSERT INTO products (tem, cost) VALUES ("Coffee Beans", 8);
+INSERT INTO products (tem, cost) VALUES ("Broccoli", 3);
+INSERT INTO products (tem, cost) VALUES ("Carrots", 2);
 
-INSERT INTO order_products VALUES (1, 1, 100);
-INSERT INTO order_products VALUES (2, 5, 50);
-INSERT INTO order_products VALUES (3, 8, 25);
+INSERT INTO order_products (orderID, productID, quantity) VALUES (1, 100);
+INSERT INTO order_products (orderID, productID, quantity) VALUES (5, 50);
+INSERT INTO order_products (orderID, productID, quantity) VALUES (8, 25);
 
